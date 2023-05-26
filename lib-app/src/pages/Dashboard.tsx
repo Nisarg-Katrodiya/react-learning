0450d87925ef741c055906691f767498e1232b45
Nisarg AC <nisargkatrodiya00017@gmail.com>
2023-05-26 18:18:19 +0530

    Added Dashboard

--- lib-app/src/pages/Dashboard.tsx
+++ lib-app/src/pages/Dashboard.tsx
@@ -1,8 +1,53 @@
 import {FC} from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import {getDataFromSession} from '../utils/localstorage';
 
 const Dashboard : FC = () => {
+  const userData = getDataFromSession('currentUser');
   return (
-    <div>Dashboard</div>
+    <>
+    <Card>
+      <CardContent>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <ListItemText primary="Name: " />
+            </ListItemAvatar>
+            <ListItemText primary={userData.name}/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <ListItemText primary="Email: " />
+            </ListItemAvatar>
+            <ListItemText primary={userData.email}/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <ListItemText primary="Phone: " />
+            </ListItemAvatar>
+            <ListItemText primary={userData.number || '-'}/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <ListItemText primary="In care of: " />
+            </ListItemAvatar>
+            <ListItemText primary={userData.parentUser || '-'}/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <ListItemText primary="Role: " />
+            </ListItemAvatar>
+            <ListItemText primary={userData.role}/>
+          </ListItem>
+        </List>
+      </CardContent>
+    </Card>
+    </>
   )
 }
 
