eb6f08af76fce189e531b6e5eb7445eb3d56f2c5
Nisarg AC <nisargkatrodiya00017@gmail.com>
2023-06-06 09:45:53 +0530

    Created branch for lib-app

--- lib-api/src/utils/responseCode.js
+++
@@ -1,8 +0,0 @@
-
-exports.success = 200;
-exports.badRequest = 400;
-exports.internalServerError = 500;
-exports.unAuthorizedRequest = 401;
-exports.notFound = 404;
-exports.conflict = 409;
-exports.validationError = 422;
